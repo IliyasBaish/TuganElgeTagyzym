8ece386c5a7d99698a994a644cc9655b165bac07
IliyasBaish <iliyasbaish@gmail.com>
2022-09-10 19:54:59 +0600

    demo

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
-var headerText = document.getElementById("header-text");
-var headerCover = document.getElementById("text-cover");
-//document.getElementById("text-cover").textContent = headerText.offsetWidth + " " + headerText.offsetHeight;
-headerCover.style.width = headerText.offsetWidth + "px";
-headerCover.style.height = headerText.offsetHeight + "px";
-headerCover.style.marginTop = headerText.offsetHeight + "px";
+function arrowAnim() {
+  document.getElementById('black-l').style.animation = document.getElementById('black-l').dataset.animate;
+  console.log(document.getElementById('black-l').dataset.animate)
+}
 
-const newspaperSpinning = [
-    { marginTop: '0px' },
-    { marginTop: headerText.offsetHeight + "px" }
-  ];
-  
-const newspaperTiming = {
-    duration: 2000,
-    iterations: 1,
-    easing: 'ease-in-out'
-  }
-
-headerCover.animate(newspaperSpinning, newspaperTiming);
-
-document.addEventListener("resize", function(){
-    headerCover.style.width = headerText.offsetWidth + "px";
-    headerCover.style.height = headerText.offsetHeight + "px";
-    headerCover.style.marginTop = headerText.offsetHeight + "px";
-})
--- test/test.js
+++ test/test.js
@@ -1,16 +1,55 @@
-const newspaperSpinning = [
-    { transform: 'rotate(0) scale(1)' },
-    { transform: 'rotate(360deg) scale(0)' }
-  ];
-  
-  const newspaperTiming = {
-    duration: 2000,
-    iterations: 1,
-  }
-  
-  const newspaper = document.querySelector(".newspaper");
-  
-  //newspaper.addEventListener('click', () => {
-    newspaper.animate(newspaperSpinning, newspaperTiming);
-  //});
-  
+arrow = false
+
+const callback = (entries) => {
+    entries.forEach(
+        (entry) => {
+            if(entry.isIntersecting) {
+                console.log("The element is intersecting >");
+                entry.target.style.animation = entry.target.dataset.animate;
+            } else {
+                entry.target.style.animation="none"
+            }
+        }
+    )
+}
+
+let observer = new IntersectionObserver(callback);
+
+const animationItems = document.querySelectorAll('.scroll-anim');
+
+animationItems.forEach(item => {
+    observer.observe(item)
+})
+
+function scrollToTop() {
+    document.getElementById("top-link").scrollIntoView();
+}
+
+const topHeaderObs = (entries) => {
+    entries.forEach(
+        (entry) => {
+            if(entry.isIntersecting) {
+                document.getElementById("arrow").style.animation = document.getElementById("arrow").dataset.animate;
+                arrow = true
+            } else {
+            }
+        }
+    )
+}
+
+let observer1 = new IntersectionObserver(topHeaderObs);
+observer1.observe(document.getElementById("text-one"))
+
+const topHeaderObsRev = (entries) => {
+    entries.forEach(
+        (entry) => {
+            if(entry.isIntersecting && arrow) {
+                document.getElementById("arrow").style.animation = "arrow-appearing-rev 0.5s forwards"
+            } else {
+            }
+        }
+    )
+}
+
+let observer2 = new IntersectionObserver(topHeaderObsRev);
+observer2.observe(document.getElementById("header-text"))
