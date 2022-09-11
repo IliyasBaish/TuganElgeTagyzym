2d75fc9fb6db58b505a332e34365f3481d05b6de
IliyasBaish <iliyasbaish@gmail.com>
2022-09-11 18:57:27 +0600

    arrow issue solve

--- test/test.js
+++ test/test.js
@@ -4,7 +4,6 @@ const callback = (entries) => {
     entries.forEach(
         (entry) => {
             if(entry.isIntersecting) {
-                console.log("The element is intersecting >");
                 entry.target.style.animation = entry.target.dataset.animate;
             } else {
                 entry.target.style.animation="none"
@@ -25,27 +24,16 @@ function scrollToTop() {
     document.getElementById("top-link").scrollIntoView();
 }
 
-const topHeaderObs = (entries) => {
-    entries.forEach(
-        (entry) => {
-            if(entry.isIntersecting) {
-                document.getElementById("arrow").style.animation = document.getElementById("arrow").dataset.animate;
-                arrow = true
-            } else {
-            }
-        }
-    )
-}
-
-let observer1 = new IntersectionObserver(topHeaderObs);
-observer1.observe(document.getElementById("text-one"))
-
 const topHeaderObsRev = (entries) => {
     entries.forEach(
         (entry) => {
-            if(entry.isIntersecting && arrow) {
+            if(entry.isIntersecting) {
+                console.log("intersecting")
                 document.getElementById("arrow").style.animation = "arrow-appearing-rev 0.5s forwards"
-            } else {
+            } else if(!entry.isIntersecting) {
+                console.log("not intersecting")
+                document.getElementById("arrow").style.animation = document.getElementById("arrow").dataset.animate;
+                //arrow = true
             }
         }
     )
