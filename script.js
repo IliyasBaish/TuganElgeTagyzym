715f4c6170229d7478435d62c2b6b2613fbdaafd
IliyasBaish <iliyasbaish@gmail.com>
2022-09-14 20:58:14 +0600

    Map

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function arrowAnim() {
-  document.getElementById('black-l').style.animation = document.getElementById('black-l').dataset.animate;
-  console.log(document.getElementById('black-l').dataset.animate)
+let start = Date.now(); // запомнить время начала
+
+let times = 1
+
+let timer = setInterval(function() {
+  if (times == 21) {
+    document.getElementById("place-bttn-1").style.opacity = "1";
+    return
+  }
+  
+  document.getElementById("map").src = "/map-img/map" + String(times) + ".png"
+
+  times = times + 1
+
+}, 500);
+
+function onPlaceBttnClick() {
+  console.log("test")
+  let entry = document.getElementById("map-container")
+  entry.style.animation = entry.dataset.animate;
 }
+
 
